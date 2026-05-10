2c2824341bde712c0612a9ee79474ede4f6d5b59
User <user@example.com>
2026-05-10 20:00:38 +0800

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import createCssPlugin from './plugins/css-plugin.js';
 
 export default defineConfig({
+  base: '/v-scroll/',
   plugins: [createCssPlugin()],
   build: {
     outDir: 'dist',
